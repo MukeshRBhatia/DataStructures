93c88ab7cb085eeaa48820ce35bf6219176f8342
Mukesh Bhatia <bhatiamax@gmail.com>
2023-03-26 20:03:17 -0400

    update

--- src/Linkedlist/SingleLinkedList.java
+++ src/Linkedlist/SingleLinkedList.java
@@ -19,6 +19,11 @@ public class SingleLinkedList {
 		Node(int value) {
 			this.value = value;
 		}
+
+		@Override
+		public String toString() {
+			return "Node [value=" + value + "]";
+		}
 	}
 
 	public void append(int value) {
@@ -34,30 +39,189 @@ public class SingleLinkedList {
 	}
 
 	public void printLinkedList() {
+		if(length == 0) {
+			System.out.println("The linkedlist is empty");
+			return;
+		}
 		Node temp = this.head;
 		while(temp != null) {
-			System.out.println(temp.value);
+			System.out.print(temp.value +"->");
 			temp = temp.next;
 		}	
+		System.out.println();
 	}
 	
-	public void removeLast() {
+	
+	/* Last element cannot be remove directly as we do not know the tail after removing the last node.
+	 * So we iterate from first and maintain last and the penultimate last node
+	 * Complexity O(n)
+	 * 
+	 * */
+	public Node removeLast() {
+		if(length == 0) {
+			System.out.println("The linkedlist is empty");
+			return null;
+		}
+		Node temp = head;
+		Node pretemp = head;
 		
+		while(temp.next != null) {
+			pretemp = temp;
+			temp = temp.next;
+		}
+		tail = pretemp;
+		pretemp.next = null;
+		length--;
+		if(length == 0) {
+			this.tail = null;
+			this.head = null;
+		}
+		return temp;
 	}
 	
-	public void removeFirst() {
-		
+	/* Last element cannot be remove directly as we do not know the tail after removing the last node.
+	 * So we iterate from first and maintain last and the penultimate last node
+	 * Complexity O(n)
+	 * 
+	 * */
+	public Node removeFirst() {
+		if(length == 0) {
+			System.out.println("The linkedlist is empty");
+			return null;
+		}
+		if(length == 1) {
+			tail = null;
+			head = null;
+		}
+		Node first = head;
+		head = head.next;
+		first.next = null;
+		length--;
+		if(length == 0) {
+			tail = null;
+			head = null;
+		}
+		return first;
+	}
+	
+	public void prepend(int value) {
+		Node newNode = new Node(value);
+		if(length == 0) {
+			tail = newNode;
+			head = newNode;
+		} else {
+			newNode.next = head;
+			head = newNode;
+		}
+		length++;
 	}
 
-	public static void main(String[] args) {
-		SingleLinkedList ll = new SingleLinkedList(10);
-		ll.append(23);
-		ll.append(88);
-		ll.append(230);
-		ll.append(808);
-		ll.append(231);
-		ll.append(8448);
-		ll.printLinkedList();
+	public Node get(int index) {
+		//Zero based indexing
+		if(index < 0 || index >= length) {
+			return null;
+		}
+		Node temp = head;
+		for(int i = 0; i < index; i++) {
+			temp = temp.next;
+		}
+		return temp;
 	}
-
+	
+	//This method updates the value at a particular index
+	public boolean set(int index, int value) {
+		//Zero based indexing
+		if(index < 0 || index >= length) {
+			return false;
+		}
+		Node temp = head;
+		for(int i = 0; i < index; i++) {
+			temp = temp.next;
+		}
+		temp.value = value;
+		return true;
+	}
+	
+	//This method inserts a new node at a particular index
+	public boolean insert(int index, int value) {
+		//Zero based indexing
+		if (index < 0 || index > length) return false;
+		if (index == 0) {
+			prepend(value);
+			return true;
+		}
+		if (index == length) {
+			append(value);
+			return true;
+		}
+		Node newNode = new Node(value);
+		Node temp = get(index - 1);
+		newNode.next = temp.next;
+		temp.next = newNode;
+		length++;
+		return true;
+	}
+	
+	//This method removes a node at a particular index
+	public Node remove(int index) {
+		//Zero based indexing
+		if (index < 0 || index >= length) return null;
+		if (index == 0) return removeFirst();
+		if (index == length - 1) return removeLast();
+		Node temp = get(index - 1);
+		Node remove = temp.next;
+		temp.next = remove.next;
+		remove.next = null;
+		length--;
+		return remove;
+	}
+	
+	public void reverse() {
+		//first swap head and tail
+		Node temp = head;
+		head = tail;
+		tail = temp;
+		//now we need 3 pointers - before, temp, after
+		Node after = temp.next;
+		Node before = null;
+		// before the iteration starts, before = null, temp = node1, after = node2  
+		// So we reverse the next value in temp to before
+		for (int i = 0; i < length; i++) {
+			after = temp.next;
+			temp.next = before;
+			before = temp;
+			temp = after;
+		}
+	}
+	
+	
+	// finds middle node without considering the length variable
+	// makes 2 variables.
+	// Approach is when the faster pointer reaches the end, the slower pointer is still at the middle
+	public Node findMiddleNode(){
+	    if(head == null) return null;
+	    Node temp = head;
+	    Node temp1 = head;
+	    while(temp.next != null) {
+	        temp1 = temp1.next;
+	        temp = temp.next.next;
+	    }
+	    return temp1;
+	}
+	
+	public Node findKthFromEnd(int k) {
+        Node slow = this.head;
+        Node fast = this.head;
+        for (int i = 0; i < k; i++) {
+            if (fast == null) {
+                return null;
+            }
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
 }
---
+++ src/Linkedlist/SinglyLinkedListUtility.java
@@ -0,0 +1,47 @@
+package linkedlist;
+
+public class SinglyLinkedListUtility {
+	
+	public static void main(String[] args) {
+		SingleLinkedList ll = new SingleLinkedList(10);
+		ll.append(23);
+		ll.append(88);
+		ll.append(230);
+		ll.append(808);
+		ll.append(231);
+		ll.append(8448);
+		ll.printLinkedList();
+		System.out.println("removed last - " + ll.removeLast());
+		System.out.println("removed last - " + ll.removeLast());
+		System.out.println("removed last - " + ll.removeLast());
+		System.out.println("removed last - " + ll.removeLast());
+		System.out.println("removed last - " + ll.removeLast());
+		System.out.println("removed last - " + ll.removeLast());
+		ll.printLinkedList();
+		ll.append(8448);
+		ll.printLinkedList();
+		ll.removeFirst();
+		ll.printLinkedList();
+		ll.prepend(222);
+		ll.prepend(111);
+		ll.printLinkedList();
+		System.out.println(ll.get(0));
+		System.out.println(ll.get(1));
+		System.out.println(ll.get(2));
+		System.out.println(ll.get(3));
+		System.out.println(ll.get(4));
+		ll.set(1, 101010);
+		ll.printLinkedList();
+		ll.insert(1, 555);
+		ll.insert(1, 5055);
+		ll.insert(1, 50055);
+		ll.insert(1, 500055);
+		ll.printLinkedList();
+		ll.remove(1);
+		ll.remove(1);
+		ll.printLinkedList();
+		ll.reverse();
+		ll.printLinkedList();
+	}
+
+}
